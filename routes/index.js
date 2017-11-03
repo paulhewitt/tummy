0c5bff34fe1a27693f5eb83806a8368cc153ffab
Paul Hewitt <thephewitt@gmail.com>
2017-11-02 23:48:00 -0600

    updated formatting

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Tummy' });
+router.get('/', function(req, res) {
+	res.render('index', { title: 'Tummy | Login' });
 });
 
 module.exports = router;
